ac522748775610f3b7ec672d0c34d8ef12a52654
Annie Huang <annie.hn@gmail.com>
2020-08-27 20:46:29 +1000

    Copy all global variables from the script.js into App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import Figure from "./components/Figure";
 import WrongLetters from "./components/WrongLetters";
 import Word from "./components/Word";
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+let playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
+
 function App() {
   return (
     <>
