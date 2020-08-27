c639ddb6efa72f2d7016238268660ee1599098dc
Annie Huang <annie.hn@gmail.com>
2020-08-27 21:03:55 +1000

    Copy the logic of displayWord in the script.js into Word.js

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const Word = () => {
+const Word = ({selectedWord, correctLetters}) => {
   return (
-    <div className="word" id="word"></div>
+    <div className="word">
+      {selectedWord.split('').map((letter, i) => (
+        <span className="letter" key={i}>
+          {correctLetters.includes(letter) ? letter : ''}
+        </span>
+      ))}
+    </div>
   );
 };
 
